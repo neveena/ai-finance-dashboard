17db219c7099858ac78ca6ca49c4fce0d299ffef
neveena <neveenaferrao@gmail.com>
2025-01-05 16:00:06 -0500

    chore: code cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
